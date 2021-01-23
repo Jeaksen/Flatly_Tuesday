ab437b581cc1ac4d806d0f7b31c2921a576d9ad0
MateuszZalewski0311 <mateusz.zalewski0311@gmail.com>
2021-01-23 19:48:46 +0100

    Small Changes to Routing v2

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -19,25 +19,46 @@ function App() {
   return (
     <Provider store = {store}>
       <Router>
-        <Switch>
-          <Route path={`/bookings/details/:bookingId`}>
-            <BookingDetails />
-          </Route>
-          <Route path="/bookings">
-            <BookingsList />
-          </Route>
-          <Route path={`/flats/details/:flatId`}>
-            <span>Flat Details Page</span>
-          </Route>
-          <Route path="/flats">
-            <span>Flats List Page</span>
-          </Route>
-          <Route path="/">
-            <span>Login Page</span>
-            <Link to="/bookings">Bookings</Link>
-            <Link to="/flats">Flats</Link>
-          </Route>
-        </Switch>
+        <div>
+          <span>FLATLY</span>
+          <ul>
+            <li>
+              <Link to="/bookings">
+                Bookings
+              </Link>
+            </li>
+            <li>
+              <Link to="/flats">
+                Flats
+              </Link>
+            </li>
+            <li>
+              <button>Log Out</button>
+            </li>
+          </ul>
+          <Switch>
+            <Route path={`/bookings/details/:bookingId`}>
+              <BookingDetails />
+            </Route>
+            <Route path="/bookings">
+              <BookingsList />
+            </Route>
+            <Route path="/flats/add">
+              <span>Add New Flat Page</span>
+            </Route>
+            <Route path={`/flats/details/:flatId`}>
+              <span>Flat Details Page</span>
+            </Route>
+            <Route path="/flats">
+              <span>Flats List Page</span>
+            </Route>
+            <Route path="/">
+              <span>Login Page</span>
+              <Link to="/bookings">Bookings</Link>
+              <Link to="/flats">Flats</Link>
+            </Route>
+          </Switch>
+        </div>
       </Router>
     </Provider>
   );
