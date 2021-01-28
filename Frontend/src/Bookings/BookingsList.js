e98140daff9e0ed4c356359e461e49af965e8b3f
MateuszZalewski0311 <mateusz.zalewski0311@gmail.com>
2021-01-28 20:00:49 +0100

    Added Base for Filtering

--- Frontend/src/Bookings/BookingsList.js
+++ Frontend/src/Bookings/BookingsList.js
@@ -1,8 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Select from 'react-select';
+import { DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 import { connect } from 'react-redux';
-import { loadBookingsListAsync, cancelBooking } from './Actions/bookingsListActions'
+import { loadBookingsListAsync, cancelBooking } from './Actions/bookingsListActions';
 import BookingsListItem from "./BookingsListItem";
-import "./BookingsList.css"
+import "./BookingsList.css";
 
 const mapStateToProps = (state, ownProps) => ({ 
     bookings: state.bookingsList.list,
@@ -18,23 +22,98 @@ const mapDispatchToProps = (dispatch) => ({
 
 function BookingsList(props)
 {   
+    const [size, setSize] = useState(10);
+    const [page, setPage] = useState(0);
+    const [sort, setSort] = useState("");
+    const [sortDir, setSortDir] = useState("desc");
+    const [name, setName] = useState("");
+    const [city, setCity] = useState("");
+    const [country, setCountry] = useState("");
+    const [date, setDate] = useState([
+        {
+          startDate: new Date(),
+          endDate: new Date(),
+          key: 'selection'
+        }
+      ]);
+
+    const countryOptions = [
+        { value: 'poland', label: 'Poland' },
+        { value: 'england', label: 'England' },
+        { value: 'russia', label: 'Russia' },
+        { value: 'arstotzka', label: 'Arstotzka' }
+    ]
+    const cityOptions = [
+        { value: 'warsaw', label: 'Warsaw' },
+        { value: 'moscow', label: 'Moscow' },
+        { value: 'london', label: 'London' }
+    ]
+
     useEffect(() => {props.loadBookingsListAsync()}, [])
-    if (props.loading) {
-        return (<label>Loading...</label>)
-    }
-    if (props.error) {
-        return (<label>Fetch error</label>)
-    }
-    if (props.bookings && props.bookings.length > 0)
-        return (
-            <div className="BookingsListPanel">
+    return (
+        <div className="BookingsListPanel">
+            <ul className="BookingsFilterPanel">
+                <SingleSelect 
+                    name="countrySelect"
+                    placeholder="Select Country..."
+                    options={countryOptions}
+                    onChange={(opt) => { opt != null ? setCountry(opt.value) : setCountry("")}}
+                />
+                <SingleSelect 
+                    name="citySelect"
+                    placeholder="Select City..."
+                    options={cityOptions} onChange={(opt) => { opt != null ? setCity(opt.value) : setCity("")}}
+                />
+                <DateRange
+                    editableDateInputs={true}
+                    onChange={item => setDate([item.selection])}
+                    moveRangeOnFirstSelection={false}
+                    ranges={date}
+                    color={"#ff9900"}
+                />
+                <input className="FlatNameInput" 
+                    placeholder="Search by Flat's Name"
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <button onClick={() => {
+                    let opt_str = `?size=${size}&page=${page}`;
+                    if (sort !== "") opt_str += `&sort=${sort}&${sort}.dir=${sortDir}`;
+                    if (name !== "") opt_str += `&name=${name}`;
+                    if (country !== "") opt_str += `&country=${country}`;
+                    if (city !== "") opt_str += `&city=${city}`;
+                    console.log(date);
+                    console.log(opt_str);
+                    props.loadBookingsListAsync();
+                }}>
+                    Apply Filters
+                </button>
+            </ul>
+            {props.loading ? <label>Loading...</label> : props.error ? <label>Fetch error</label> : props.bookings && props.bookings.length > 0 ?  
                 <ul className="BookingsList">{props.bookings.map((booking) => {
                     return <BookingsListItem key={booking.id} booking={booking} cancelBooking={props.cancelBooking} />})}
-                </ul>
-            </div>
-        )
-    return <div />
+                </ul> :
+            <div />}
+        </div>
+    );
 }
 
+function SingleSelect (props)
+{
+    return(
+        <Select
+          className="single-select"
+          classNamePrefix="select"
+          isDisabled={false}
+          isLoading={false}
+          isClearable={true}
+          isRtl={false}
+          isSearchable={true}
+          name={props.name}
+          placeholder={props.placeholder}
+          options={props.options}
+          onChange={props.onChange}
+        />
+    );
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(BookingsList);
