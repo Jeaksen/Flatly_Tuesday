65d6940ce0a45a29580e777239f26541dbeed00d
Pawel Pohl <pawel.pohl229@gmail.com>
2021-02-08 15:55:00 +0100

    Inactive bookings filtering bug fixed.

--- Backend/src/main/java/pw/react/backend/dao/BookingsRepository.java
+++ Backend/src/main/java/pw/react/backend/dao/BookingsRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import pw.react.backend.model.Booking;
 
+import java.util.List;
+
 public interface BookingsRepository extends JpaRepository<Booking, Long>, JpaSpecificationExecutor<Booking>
 {
+    List<Booking> findByActive(boolean active);
 }
